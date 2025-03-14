438d799b9bf8071cf102ffd8edc6680cedfe0697
刘重量 <39993639+xinghanking@users.noreply.github.com>
2025-03-15 02:06:49 +0800

    init

--- app/Http/Middleware/Auth.php
+++ app/Http/Middleware/Auth.php
@@ -24,18 +24,18 @@ class Auth
         if(!session()->has('uid')) {
             $authorization = $request->header('Authorization');
             if (empty($authorization) || !str_contains($authorization, 'Basic ')) {
-                return response()->json(['error: Unauthorized'], 401)->header('WWW-Authenticate', 'Basic realm="Restricted Area"');
+                return response()->json(['error: Unauthorized'], Response::HTTP_UNAUTHORIZED)->header('WWW-Authenticate', 'Basic realm="Restricted Area"');
             }
             $authorization = base64_decode(substr($authorization, 6));
             [$username, $password] = explode(':', $authorization, 2);
             $info = User::getInstance()->getInfoByUserAndPass($username, md5($authorization));
             if (empty($info)) {
-                return response()->json(['error: Unauthorized'], 401)->header('WWW-Authenticate', 'Unauthorized');
+                return response()->json(['error: Unauthorized'], Response::HTTP_UNAUTHORIZED)->header('WWW-Authenticate', 'Unauthorized');
             }
             session(['uid' => $info['id'], 'username' => $info['username'], 'email' => $info['email']]);
         }
         if(!in_array($request->getMethod(), ['OPTIONS', 'PROPFIND']) && !str_starts_with($request->getRequestUri(), '/' . session('username') . '/calendars/')) {
-            return response(''，  Response::HTTP_FORBIDDEN);
+            return response('',  Response::HTTP_FORBIDDEN);
         }
         return $next($request);
     }
